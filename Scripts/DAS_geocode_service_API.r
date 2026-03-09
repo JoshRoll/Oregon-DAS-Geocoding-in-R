29b7312ea880058f1794f3d98ec2482af2a5fce0
JoshRoll <josh.f.roll@odot.state.or.us>
2026-03-09 16:55:25 -0700

    Add files via upload

--- Scripts/DAS_geocode_service_API.r
+++ Scripts/DAS_geocode_service_API.r
@@ -3,10 +3,10 @@
 #Description: This script uses DAS's geocoding service API to geocode address data
 
 #Notes:
+#Updated 3/9/2026
 
   
-	#Ensure proper directory for libraries
-	.libPaths("C:/Program Files/R/R-4.0.2/library")
+	
 	#Load libraries
 	library(tidyr)
 	library(stringr)
@@ -33,11 +33,102 @@
 	}
 	#Function to send a batch of addresses for geocoding
 	DAS_geocode_Call <- function(Address){
-        TempCall <- paste0("https://navigator.state.or.us/arcgis/rest/services/Locators/gc_Composite/GeocodeServer/findAddressCandidates?Street=&City=&State=&ZIP=&SingleLine=+",Address,"&category=&outFields=*&maxLocations=&outSR=&searchExtent=&location=&distance=&magicKey=&f=pjson")
+        #TempCall <- paste0("https://navigator.state.or.us/arcgis/rest/services/Locators/gc_Composite/GeocodeServer/findAddressCandidates?Street=&City=&State=&ZIP=&SingleLine=+",Address,"&category=&outFields=*&maxLocations=&outSR=&searchExtent=&location=&distance=&magicKey=&f=pjson")
+		TempCall <- paste0("https://navigator.state.or.us/arcgis/rest/services/Locators/OregonAddress/GeocodeServer?Street=&City=&State=&ZIP=&SingleLine=+",Address,"&category=&outFields=*&maxLocations=&outSR=&searchExtent=&location=&distance=&magicKey=&f=pjson")
 		rest <- GET(TempCall)
 		tempExtract <- fromJSON(rawToChar(rest$content), flatten = T)
 		dfOut <- data.frame(tempExtract$candidates)    
 	}
+	
+	
+	#Write new function to call API
+	###################################
+	library(httr2)
+	library(jsonlite)
+	library(dplyr)
+
+	DAS_geocode_Call <- function(address_text,
+                             base_url = "https://navigator.state.or.us/arcgis/rest/services/Locators/OregonAddress/GeocodeServer/findAddressCandidates") {
+		  
+		  out <- tryCatch({
+			
+			resp <- httr2::request(base_url) |>
+			  httr2::req_url_query(
+				SingleLine = address_text,
+				f = "json",
+				maxLocations = 1
+			  ) |>
+			  httr2::req_perform()
+			
+			dat <- jsonlite::fromJSON(httr2::resp_body_string(resp), simplifyDataFrame = TRUE)
+			
+			# If no candidates returned
+			if (nrow(dat$candidates) == 0) {
+			  
+			  return(data.frame(
+				address = "No Address",
+				location.x = NA,				
+				location.y = NA,
+				score = NA,
+				extent.xmin = NA,
+				extent.ymin = NA,
+				extent.xmax = NA,
+				extent.ymax = NA,
+				stringsAsFactors = FALSE
+			  ))
+			}
+			
+			cand <- dat$candidates
+			
+			if (!is.data.frame(cand)) {
+			  cand <- as.data.frame(cand)
+			}
+			
+			cand <- jsonlite::flatten(as.data.frame(dat$candidates))
+			rownames(cand) <- NULL
+						
+			cand
+			
+		  }, error = function(e) {
+			
+			# Return fallback record on failure
+			data.frame(
+				address = "No Address",
+				location.x = NA,				
+				location.y = NA,
+				score = NA,
+				extent.xmin = NA,
+				extent.ymin = NA,
+				extent.xmax = NA,
+				extent.ymax = NA,
+				stringsAsFactors = FALSE
+			)
+		  })
+		  
+		  out
+		}
+	
+	
+	
+	
+	
+	library(httr2)
+library(jsonlite)
+
+base_url <- "https://navigator.state.or.us/arcgis/rest/services/Locators/OregonAddress/GeocodeServer/findAddressCandidates"
+
+"20400 SW TETON AVE TUALATIN OR 97062"
+resp <- request(base_url) |>
+  req_url_query(
+    SingleLine = "20400 SW TETON AVE TUALATIN OR 97062",
+    f = "json"
+  ) |>
+  req_perform()
+
+txt <- resp_body_string(resp)
+dat <- fromJSON(txt, simplifyDataFrame = TRUE)
+
+dat$candidates
 
 #Define useful script objects
 #---------------------------
@@ -45,7 +136,7 @@
 	Projection <-"+proj=utm +zone=10 +datum=WGS84"
   
 	#Define working directory
-	setwd("//wpdotfill09/R_VMP3_USERS/tdb069/Sandbox/Geocoding")
+	setwd("F:/Sandbox/Geocoding")
 	
 	#Check to see if output and Results directory exists
 	if(!(file.exists("Output"))){dir.create("Output")}
@@ -60,7 +151,7 @@
 	#Make a copy of the data
 	Data.. <- Load_Data.. 
 	#Rename address column - note you will need to use paste0 to concatenate a multipart address field
-	Data.. <-  mutate(Data..,Address = Owner_Address)
+	Data.. <-  mutate(Data.., Address = Owner_Address)
 	#Create an id
 	Data..$Id <- 1:nrow(Data..)
 
@@ -75,43 +166,91 @@
 	
 	#Set up progress bar
 	#############################
-	pb <- txtProgressBar(min=1, max=length(Chunks_), style=3)
+	# Set up progress bar
+	pb <- txtProgressBar(min = 1, max = length(Chunks_), style = 3)
 	progress <- function(n) setTxtProgressBar(pb, n)
-	opts <- list(progress=progress)
-	#Start a timer
+
+	# Start timer
 	Main_Start_Time <- Sys.time()
-	#Init process
-	for(i in 1:length(Chunks_)){
-		Start_Time <- Sys.time()
-		#Create Id
-		Id <-  Chunks_[[i]]
-		#Determine Premise IF
-		Record_Id <- Data..$Id[Id]
-		#Develop query 
-		Address <- gsub(" ","+",Data..$Address[Id])
-		#Remove pound signs
-		Address <- gsub("#","",Address)
-		State <- Data..$State[Id]
-		#Check to make sure address is present - to ensure all fields spec outFields as outFields=*
-		if(all(!(is.na(Address)) & State%in%"OR")){
-			#Batch call 
-			Api_Call_Result_ <- lapply(  Address, DAS_geocode_Call)
+
+	for (i in seq_along(Chunks_)) {
+	  
+	  Start_Time <- Sys.time()
+	  
+	  # Current chunk of row indices
+	  Id <- Chunks_[[i]]
+	  
+	  # Subset data for this chunk
+	  chunk_dat <- Data..[Id, , drop = FALSE]
+	  
+	  # Keep original row ids
+	  chunk_dat$Id <- Data..$Id[Id]
+	  
+	  # Clean address
+	  chunk_dat$Address_clean <- gsub("#", "", chunk_dat$Address)
+	  
+	  # Keep only valid Oregon records with non-missing address
+	  chunk_dat_valid <- chunk_dat %>%
+		filter(
+		  !is.na(Address_clean),
+		  Address_clean != "",
+		  !is.na(State),
+		  State == "OR"
+		)
+	  
+	  # Initialize output for this chunk
+	  Result.. <- data.frame()
+	  
+	  # Call API only if there are valid records
+	  if (nrow(chunk_dat_valid) > 0) {
+		
+		Api_Call_Result_ <- lapply(chunk_dat_valid$Address_clean, DAS_geocode_Call)
+		
+		# Keep only non-empty results
+		has_rows <- sapply(Api_Call_Result_, nrow) > 0
+		
+		if (any(has_rows)) {
+			Api_Call_Result_ <- Api_Call_Result_[has_rows]
+
+			# Name list elements by source Id
+			names(Api_Call_Result_) <- chunk_dat_valid$Id[has_rows]
+			# Combine and attach Id
+			Result.. <- dplyr::bind_rows(
+			lapply(seq_along(Api_Call_Result_), function(j) {
+				out <- Api_Call_Result_[[j]]
+				out <- as.data.frame(out)
+				rownames(out) <- NULL
+				out$Id <- names(Api_Call_Result_)[j]
+				out
+			})
+			)
 		}
-		#Check to make sure there are results
-		if(length(Api_Call_Result_)>0){
-		  #Name the list to maintain the Ids
-		  names(Api_Call_Result_) <- Id
-		  #Convert to data frame and append Ids
-		  Result.. <- cbind(do.call("rbind",Api_Call_Result_), Id = rep(names(Api_Call_Result_), sapply(Api_Call_Result_, nrow)))
-		}
-		#Write out results to avoid problems with Geocoder not finishing 
-		save(Result.., file = paste(Write_Off_Dir, "Chunk_",min(Id),"_",max(Id),".RData",sep=""))
-		#Print progress 
-		print(paste0("Chunk ",i," Done"))
-		print(Sys.time() - Start_Time)		  
-	#Close for loop
+	  }
+	  
+	  # Save chunk output even if empty
+	  save(
+		Result..,
+		file = paste0(
+		  Write_Off_Dir,
+		  "Chunk_",
+		  min(Id),
+		  "_",
+		  max(Id),
+		  ".RData"
+		)
+	  )
+	  
+	  # Update progress
+	  progress(i)
+	  
+	  print(paste0("Chunk ", i, " Done"))
+	  print(Sys.time() - Start_Time)
 	}
-    #Record end time
+
+	# Close progress bar
+	close(pb)
+
+	# Total elapsed time
 	Sys.time() - Main_Start_Time
   
 	#Compile the chunks and select only the address points that are based on 
@@ -133,13 +272,13 @@
 	#Load results
 	Results.. <- assignLoad(file =  paste0("Results/Compiled_Results.RData"))
 	#Create a spatial file - remove records with no UTM X coordiante and are not a reliable point 
-	Results.. <- 	filter(Results.. ,!is.na(attributes.X) & attributes.Loc_name%in%c("ADDRESSPOINT","NAVTEQ","ORTRANS") ) 
+	Results.. <- 	filter(Results.. ,!is.na( location.x )  ) 
 	#Select point with highest score - ignores ties and selects the highest score
-	Results.. <- Results..%>% group_by(Id) %>% slice_max(order_by = attributes.Score, n = 1, with_ties =F) 
+	Results.. <- Results..%>% group_by(Id) %>% slice_max(order_by = score, n = 1, with_ties =F) 
 	#Join back with original data to measure geocoding success
-	Data.. <- left_join(Data.. %>% mutate(Id = as.character(Id)), Results..[,c("Id","address","attributes.Loc_name","attributes.Score","location.x","location.y","attributes.DisplayX","attributes.DisplayY" )], by = "Id")
+	Data.. <- left_join(Data.. %>% mutate(Id = as.character(Id)) %>% dplyr::select(c(Id, Owner_Address)), Results..[,c("Id","address","score","location.x","location.y" )], by = "Id")
 	#Calculate % of record with no valid geocoded results - for example the results are 89% success, 11% unsuccessful
-	table(!is.na(Data..$attributes.Score )) / nrow(Data..)
+	table(!is.na(Data..$score )) / nrow(Data..)
 	#Examine score 
 	table(Data..$attributes.Score < 90)
 	table(Data..$attributes.Score > 76)
@@ -151,36 +290,39 @@
 	Projection_LatLong <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 	Projection_Utm <- "+proj=lcc +lat_1=43 +lat_2=45.5 +lat_0=41.75 +lon_0=-120.5 +x_0=399999.9999984 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048 +no_defs"
 	#Create a spatial data set (spatial objects denoted with _Sp)
-	Data_Sp <- SpatialPointsDataFrame(Results..[,c("attributes.X","attributes.Y")], proj4string = CRS(Projection_Utm), data = 	Results.. )
-	
-	#Transform
-	Data_Sp <- spTransform(	Data_Sp, CRS(Projection_LatLong))
-	
-	#Write out as a spatial file 
-	shapefile(Data_Sp, paste0("Results/Address"), overwrite=TRUE)
-	
-	#Create dynamic leaflet map
-	
-	#Create color pallette
-	Colors. <-  colorRampPalette(c("red","skyblue", "purple"))(3)
-	mypal <- colorFactor(Colors., domain = unique(Data_Sp@data$attributes.Loc_name))
-	#Create labels
-	Data_Sp@data$label <- with(Data_Sp@data, paste(
-		#Pedestrian Injury
+	# Create sf point object from X/Y columns
+	Data_Sf <- st_as_sf(
+		Results..,
+		coords = c("location.x", "location.y"),
+		crs = Projection_Utm,
+		remove = FALSE
+	)
+
+	# Transform to lat/long
+	Data_Sf <- st_transform(Data_Sf, crs = Projection_LatLong)
+
+	# Write out as a shapefile
+	st_write(Data_Sf, "Results/Address.shp", delete_layer = TRUE)
+
+	# Create dynamic leaflet map
+	#######################################
+	# Create labels
+	Data_Sf$label <- with(
+	  Data_Sf,
+	  paste(
 		"<p style='font-size:15px'> <b><i>", "Details", "</i></b> </br>",
-		"<b> Address: </b>",Data_Sp@data$address, "</br>",
-		"<b> Score: </b>", Data_Sp@data$attributes.Score, "</br>",
-		"<b> Geocode Location Type: </b>", Data_Sp@data$attributes.Loc_name , "</br>"))
+		"<b> Address: </b>", address, "</br>",
+		"<b> Score: </b>", score, "</br>"
+	  )
+	)
 		 
 	#Develop map 
-	Map <- leaflet(Data_Sp)%>% addTiles() %>%
-		addCircles(data=Data_Sp,  fillOpacity = .5,	color = ~mypal(attributes.Loc_name),
+	Map <- leaflet()%>% addTiles() %>%
+		addCircles(data = Data_Sf,  fillOpacity = .5,	color = "red",
 				 highlightOptions = highlightOptions(color = "white", weight = 2,   bringToFront = TRUE),
 			 popup = ~label,
-			labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px",sticky = T,interactive = T),  textsize = "15px",  direction = "auto")
-		) %>%
-	  addLegendFactor(position = "bottomright",  pal = mypal, values = Data_Sp@data$attributes.Loc_name,title = htmltools::tags$div("DAS Gecoding Service Results", style = "font-size: 24px; color: black;"),
-	  opacity = 1, width = 50, height = 50)	
+			labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px",sticky = T, interactive = T),  textsize = "15px",  direction = "auto")
+		) 
 	Map
 	#save to file - savewidget doesnt like relative paths so have to define using getwd()
 	saveWidget(Map, file=paste0(getwd(),"/Results/Dynamic_Map.html"), selfcontained = T) 
